e7f73372875e32253411f4cfc89b7826fd40775d
Sijan666 <666majharulislam@gmail.com>
2026-03-24 23:38:14 +0600

    feat : add testimonial section with responsive and use swiper js

---
+++ src/components/data/testimonialApi.js
@@ -0,0 +1,22 @@
+export const testimoni = [
+    {
+        id : 1 ,
+        review : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        name : "Johny Vino",
+    },
+    {
+        id : 2 ,
+        review : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        name : "Johny Vino",
+    },
+    {
+        id : 3 ,
+        review : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        name : "Johny Vino",
+    },
+    {
+        id : 4 ,
+        review : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        name : "Johny Vino",
+    },
+]
--- src/components/layouts/Testimonial.jsx
+++ src/components/layouts/Testimonial.jsx
@@ -1,21 +1,53 @@
-import React from 'react'
-import Container from '../Container'
+
+import Container from '../Container';
+import { testimoni } from '../data/testimonialApi';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const Testimonial = () => {
     return (
         <>
-        <section id='testimonial'>
-            <div className="bg-[linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)),url('/src/assets/reviews.png')] bg-center bg-cover bg-no-repeat pt-[45px] pb-9">
-                <Container>
-                    <div className="text-center font-roboto">
-                        <h3 className='text-white text-[30px]'>Testimonial</h3>
-                        <p className='pt-10 text-white text-[20px] px-25'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    </div>
-                </Container>
-            </div>
-        </section>
+            <section id='testimonial'>
+                <div className="bg-[linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)),url('/src/assets/reviews.png')] bg-center bg-cover bg-no-repeat pt-[45px] pb-9">
+                    <Container>
+                        <div className="text-center font-roboto">
+                            <h3 className='text-white text-[30px]'>Testimonial</h3>
+                            
+                            {/* Swiper Container */}
+                            <div className="pt-10">
+                                <Swiper
+                                    modules={[Pagination, Autoplay]}
+                                    spaceBetween={30}
+                                    slidesPerView={1}
+                                    pagination={{ clickable: true }}
+                                    autoplay={{ delay: 3000, disableOnInteraction: false }}
+                                    loop={true}
+                                    className="pb-10">
+                                    {testimoni.map((item) => (
+                                        <SwiperSlide key={item.id}>
+                                            <div className="lg:px-25 ">
+                                                <p className='text-[18px] md:text-[20px] text-white opacity-90 italic'>
+                                                    {item.review}
+                                                </p>
+                                                <h4 className='text-[20px] text-white font-bold pt-5'>
+                                                    {item.name}
+                                                </h4>
+                                            </div>
+                                        </SwiperSlide>
+                                    ))}
+                                </Swiper>
+                            </div>
+
+                        </div>
+                    </Container>
+                </div>
+            </section>
         </>
-    )
+    );
 }
 
-export default Testimonial
+export default Testimonial;
