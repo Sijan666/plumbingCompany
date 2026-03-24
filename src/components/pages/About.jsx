29861a3e0138e7eebe5a006ee81a4555823355a9
Sijan666 <666majharulislam@gmail.com>
2026-03-25 03:12:41 +0600

    feat : responsive in about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
             <div className="font-roboto pb-10">
                 <div className="bg-[url('/src/assets/About.png')] pt-[126px] pb-12 bg-center bg-cover bg-no-repeat">
                     <Container>
-                        <h3 className='uppercase text-[44px] font-black text-white'>About us</h3>
-                        <p className='pt-3.5 text-white text-sm lg:w-[432px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                        <h3 className='uppercase text-[35px] lg:text-[44px] font-black text-white text-center lg:text-left'>About us</h3>
+                        <p className='pt-3.5 text-white text-sm lg:w-[432px] text-center lg:text-left'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
                     </Container>
                 </div>
                 <div className="mt-[42px]">
