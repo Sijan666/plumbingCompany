f3d3cedc01ca4787162388eb8bb5afac492a82f7
Majharul Islam <666majharulislam@gmail.com>
2026-03-26 01:37:57 +0600

    add commenting

--- src/components/layouts/RecentWorks.jsx
+++ src/components/layouts/RecentWorks.jsx
@@ -18,6 +18,7 @@ const RecentWorks = () => {
                             <div className="one">
                                 {/* img */}
                                 <Image imgSrc={w1} className={'w-full'}/>
+                                {/* children div */}
                                 <div className="mt-[17px]">
                                     <h4 className='text-[20px] text-black'>Lorem Ipsum is simply dummy text of the printing and typesetting. </h4>
                                     <p className='text-[12px] py-3 text-black'>Don’t wrestle with rig assembly and hot, noisy miners at home. We have the fastest bitcoin mining hardware running for you....</p>
