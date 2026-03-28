7f6bf3ce73a9605a52dd56a6dbe4f5280b2b24fe
Sijan666 <666majharulislam@gmail.com>
2026-03-29 00:06:20 +0600

    make common jsx for services

---
+++ src/components/ServiceCommon.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from './Image'
+
+const ServiceCommon = ({className , serviceImg ,serviceName , serviceDesc}) => {
+    return (
+        <>
+        <div className={`${className} shadow-newMade pt-[42px] pr-[15px] pl-[18px] pb-[22px] rounded-[5px] font-roboto`}>
+            <Image className={serviceImg}/>
+            <div className="mt-[25px] text-center">
+                <h4 className='text-black font-medium text-sm'>{serviceName}</h4>
+                <p className='text-[#6D6D6D] leading-[133.5%] text-[12px] pt-[18px]'>{serviceDesc}</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default ServiceCommon
