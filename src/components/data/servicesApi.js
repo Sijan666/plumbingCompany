813c4bd99525a70618d30103687713f34b999cf4
Sijan666 <666majharulislam@gmail.com>
2026-03-29 00:44:25 +0600

    add services api

---
+++ src/components/data/servicesApi.js
@@ -0,0 +1,79 @@
+import services1 from '../../assets/services/services1.png'
+import services2 from '../../assets/services/services2.png'
+import services3 from '../../assets/services/services3.png'
+import services4 from '../../assets/services/services4.png'
+
+export const servicesApi = [
+    {
+        id : 1 ,
+        serviceImg: services1,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+    {
+        id : 2 ,
+        serviceImg: services2,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+    {
+        id : 3 ,
+        serviceImg: services3,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+    {
+        id : 4 ,
+        serviceImg: services4,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+    {
+        id : 5 ,
+        serviceImg: services2,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+    {
+        id : 6 ,
+        serviceImg: services3,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+    {
+        id : 7 ,
+        serviceImg: services4,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+    {
+        id : 8 ,
+        serviceImg: services1,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+    {
+        id : 9 ,
+        serviceImg: services2,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+    {
+        id : 10 ,
+        serviceImg: services3,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+    {
+        id : 11 ,
+        serviceImg: services4,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+    {
+        id : 12 ,
+        serviceImg: services1,
+        serviceName:"Plumbing",
+        serviceDesc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    },
+]
