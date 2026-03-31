2da0e08b19ef82dfa7b0f6aeaf0ffe459524dc6b
Sijan666 <666majharulislam@gmail.com>
2026-03-31 23:07:49 +0600

    add local api for projects

---
+++ src/components/data/projectsApi.js
@@ -0,0 +1,28 @@
+import p1 from '../../assets/p1.png'
+
+export const projectsApi = [
+    {
+        id : 1 , 
+        projectImg : p1,
+        projectType : "RESIDENTIAL",
+        projectName : "Kitchen Plumbing Project",
+    },
+    {
+        id : 1 , 
+        projectImg : p1,
+        projectType : "RESIDENTIAL",
+        projectName : "Kitchen Plumbing Project",
+    },
+    {
+        id : 2 , 
+        projectImg : p1,
+        projectType : "RESIDENTIAL",
+        projectName : "Bathroom Plumbing Project",
+    },
+    {
+        id : 3 , 
+        projectImg : p1,
+        projectType : "RESIDENTIAL",
+        projectName : "Bathroom Plumbing Project",
+    },
+]
