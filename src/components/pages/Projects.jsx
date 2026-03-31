bdd38654d1ef99d4621b12b4a9b07a6299db0300
Sijan666 <666majharulislam@gmail.com>
2026-03-31 23:08:06 +0600

    feat : add projects page

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -13,6 +13,18 @@ const Projects = () => {
                         <p className='pt-3.5 text-white text-sm lg:w-[432px] text-center lg:text-left'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
                     </Container>
                 </div>
+                <div className="mt-15">
+                    <Container>
+                        <div className="text-center">
+                            <h4 className='text-[#0E0E0E] font-medium text-[30px] uppercase'>Our Projects</h4>
+                            <p className='text-base text-[#6D6D6D] leading-[133.5%] pt-[15px] lg:px-55'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                    </Container>
+                </div>
+                {/* projects */}
+                <div className="mt-12">
+                    
+                </div>
             </div>
         </section>
         </>
