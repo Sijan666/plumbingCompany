b662d1d05dc2f1fce29cec6b671c27250665adab
Sijan666 <666majharulislam@gmail.com>
2026-03-31 23:12:22 +0600

    add local api for projects

--- src/components/data/projectsApi.js
+++ src/components/data/projectsApi.js
@@ -1,4 +1,6 @@
 import p1 from '../../assets/p1.png'
+import p2 from '../../assets/p2.png'
+import p3 from '../../assets/p3.png'
 
 export const projectsApi = [
     {
@@ -8,20 +10,50 @@ export const projectsApi = [
         projectName : "Kitchen Plumbing Project",
     },
     {
-        id : 1 , 
-        projectImg : p1,
-        projectType : "RESIDENTIAL",
-        projectName : "Kitchen Plumbing Project",
-    },
-    {
         id : 2 , 
-        projectImg : p1,
+        projectImg : p2,
         projectType : "RESIDENTIAL",
         projectName : "Bathroom Plumbing Project",
     },
     {
         id : 3 , 
+        projectImg : p3,
+        projectType : "RESIDENTIAL",
+        projectName : "Bathroom Plumbing Project",
+    },
+    {
+        id : 4 , 
+        projectImg : p2,
+        projectType : "RESIDENTIAL",
+        projectName : "Bathroom Plumbing Project",
+    },
+    {
+        id : 5 , 
+        projectImg : p3,
+        projectType : "RESIDENTIAL",
+        projectName : "Bathroom Plumbing Project",
+    },
+    {
+        id : 6 , 
         projectImg : p1,
+        projectType : "RESIDENTIAL",
+        projectName : "Kitchen Plumbing Project",
+    },
+    {
+        id : 7 , 
+        projectImg : p1,
+        projectType : "RESIDENTIAL",
+        projectName : "Kitchen Plumbing Project",
+    },
+    {
+        id : 8 , 
+        projectImg : p2,
+        projectType : "RESIDENTIAL",
+        projectName : "Bathroom Plumbing Project",
+    },
+    {
+        id : 9 , 
+        projectImg : p3,
         projectType : "RESIDENTIAL",
         projectName : "Bathroom Plumbing Project",
     },
