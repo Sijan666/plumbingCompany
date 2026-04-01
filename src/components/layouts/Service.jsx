48eeba7532c8a9fc5a1412e725c2c5d9123e64bf
Sijan666 <666majharulislam@gmail.com>
2026-04-01 12:05:51 +0600

    fix : service texts

--- src/components/layouts/Service.jsx
+++ src/components/layouts/Service.jsx
@@ -18,10 +18,7 @@ const Service = () => {
                             <div className="one text-center flex flex-col items-center">
                                 <Image imgSrc={s1} className={'mx-auto w-[50px]'}/>
                                 <h3 className='py-3 text-sm text-white font-medium'>Residental Service</h3>
-                                <p className='text-white'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.</p>
-                                <div className="btn mt-2">
-                                    <Button btnText={'Learn More'} className={'bg-white rounded-[5px] text-black text-base px-[17px] py-1.5 underline'}/>
-                                </div>
+                                <p className='text-white'>Expert plumbing solutions for your everyday home needs. From routine pipe repairs to complete installations, we ensure your household systems run smoothly and safely.</p>
                             </div>
                             {/* divider */}
                             <div className="lg:h-[225px] lg:w-px h-px w-full bg-white"></div>
@@ -29,10 +26,7 @@ const Service = () => {
                             <div className="one text-center flex flex-col items-center">
                                 <Image imgSrc={s2} className={'mx-auto w-[50px]'}/>
                                 <h3 className='py-3 text-sm text-white font-medium'>Commercial Service</h3>
-                                <p className='text-white'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.</p>
-                                <div className="btn mt-2">
-                                    <Button btnText={'Learn More'} className={'bg-white rounded-[5px] text-black text-base px-[17px] py-1.5 underline'}/>
-                                </div>
+                                <p className='text-white'>Comprehensive plumbing services designed for modern businesses. We provide scalable and reliable solutions to minimize downtime and keep your operations flowing.</p>
                             </div>
                             {/* divider */}
                             <div className="lg:h-[225px] lg:w-px h-px w-full bg-white"></div>
@@ -40,10 +34,7 @@ const Service = () => {
                             <div className="one text-center flex flex-col items-center">
                                 <Image imgSrc={s3} className={'mx-auto w-[50px]'}/>
                                 <h3 className='py-3 text-sm text-white font-medium'>Maintenance</h3>
-                                <p className='text-white'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.</p>
-                                <div className="btn mt-2">
-                                    <Button btnText={'Learn More'} className={'bg-white rounded-[5px] text-black text-base px-[17px] py-1.5 underline'}/>
-                                </div>
+                                <p className='text-white'>Proactive maintenance plans to protect your plumbing infrastructure. Regular inspections help prevent costly emergencies and extend the lifespan of your systems.</p>
                             </div>
                         </div>
                     </Container>
