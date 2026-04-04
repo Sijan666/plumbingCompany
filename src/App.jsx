63c9581e4fdefef39126e91c94f68a244ed398fe
Sijan666 <666majharulislam@gmail.com>
2026-04-04 11:31:28 +0600

    use lenis for premium smoothness

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import Home from './components/pages/Home'
-import { Routes , Route } from 'react-router-dom'
-import RootLayouts from './components/layouts/RootLayouts'
-import Error from './components/pages/Error'
-import About from './components/pages/About'
-import Services from './components/pages/Services'
-import Projects from './components/pages/Projects'
-import Contact from './components/pages/Contact'
+import Home from "./components/pages/Home";
+import { Routes, Route } from "react-router-dom";
+import RootLayouts from "./components/layouts/RootLayouts";
+import Error from "./components/pages/Error";
+import About from "./components/pages/About";
+import Services from "./components/pages/Services";
+import Projects from "./components/pages/Projects";
+import Contact from "./components/pages/Contact";
+import ReactLenis from "lenis/react";
 
-
-function App () {
+function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<RootLayouts />}>
-          <Route index element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/services" element={<Services/>} />
-          <Route path="/projects" element={<Projects/>} />
-          <Route path="/contact" element={<Contact/>} />
-        </Route>
-          <Route path="*" element={<Error/>} />
-      </Routes>
+      <ReactLenis root options={{ lerp: 0.05, duration: 1.5, smoothWheel: true, wheelMultiplier: 0.8,}}>
+        <Routes>
+          <Route path="/" element={<RootLayouts />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </ReactLenis>
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
